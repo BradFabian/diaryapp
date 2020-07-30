f1431ecd3531389a5e6c48c690f89928ee13ce05
Brad Fabian <bradleofabian@gmail.com>
2020-07-30 14:56:26 -0400

    created a non display style

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ let entryForm = document.getElementById("entryForm");
 let entriesSection = document.getElementById("entries");
 let textbox = document.querySelector(".entry-texbox");
 
-let submitButton = document.querySelector(".button");
-
 entryForm.addEventListener("submit", function (event) {
   event.preventDefault();
   let entryDiv = document.createElement("div");
   entryDiv.classList.add("single-entry");
   entryDiv.innerText = textbox.value;
+  entryDiv.style.display = "none";
   entriesSection.appendChild(entryDiv);
+
+  // clear input //
+  textbox.value = "";
 });
